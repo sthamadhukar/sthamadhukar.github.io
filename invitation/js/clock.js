a83ceac8b66e40458192e81eabea1375e98e2622
sthamadhukar <sthamadhukar@gmail.com>
2026-03-04 18:18:50 -0600

    Update clock.js

--- invitation/js/clock.js
+++ invitation/js/clock.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
   let currentDate = new Date();
 
   // Target future date/24 hour time/Timezone
-  let targetDate = moment.tz("2026-03-11 14:00","America/Chicago");
+  let targetDate = moment.tz("2026-03-11 13:30","America/Chicago");
 
   // Calculate the difference in seconds between the future and current date
   let diff = targetDate / 1000 - currentDate.getTime() / 1000;
